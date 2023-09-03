b4c5288827b47254b1852dc060decd2c86618b37
Aluon <paulacanoroman@gmail.com>
2023-09-03 17:20:24 +0200

    feat: llamadaAPI inicio componente Tabla

---
+++ consumox2-tabla-reloj/pages/screen/Listado.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import Tabla from '../components/Tabla';
+
+const Listado = () => {
+
+    const [datosAPI, setDatosAPI] = useState()
+
+    useEffect(() => {
+        llamadaAPI();
+    }, [])
+
+    const llamadaAPI = async () => {
+        try{
+        const llamada = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100&offset=0`);
+        const datos = await llamada.json();
+        // console.log(datos.results)
+        setDatosAPI(datos.results);
+        } catch (error){"error", error}
+    }
+    
+  return (
+    <>
+    <Tabla datosAPI={datosAPI} setDatosAPI={setDatosAPI}></Tabla>
+    </>
+  )
+}
+
+export default Listado
