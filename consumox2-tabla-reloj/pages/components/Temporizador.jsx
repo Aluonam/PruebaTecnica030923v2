12245a9a6daea17fd0d89ae9c92b6d2dd7c6d593
Aluon <paulacanoroman@gmail.com>
2023-09-03 18:12:27 +0200

    feat: reloj actual

--- consumox2-tabla-reloj/pages/components/Temporizador.jsx
+++ consumox2-tabla-reloj/pages/components/Temporizador.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Temporizador = () => {
+
+
+    const [hora, setHora] = useState()
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        const fecha = new Date();
+        // console.log(fecha.toLocaleString().split(",")[1])
+        setHora((fecha.toLocaleString().split(",")[1]))
+
+      }, 1000)
+
+      return()=>clearInterval(interval);
+    }, [])
+    
+    
+
   return (
-    <div>Temporizador</div>
+    <div>
+        Temporizador:
+        {hora}
+    </div>
   )
 }
 
